289f6a2ada9f2e78050babd0d56d192d5903826d
GowerCampbell <119587144+GowerCampbell@users.noreply.github.com>
2025-05-29 12:59:03 +0100

    Update solution.py

--- problems/001_two_sum/solution.py
+++ problems/001_two_sum/solution.py
@@ -1,2 +1,24 @@
+# Assumption: 
+# Taking from a list of integers to numbers that will then add together to form the targeted value.
+
+"""
+Test:
+Indexes the list by counting the total value of list items using the len function: 
+if i in range(len(nums)) 
+
+Creates a sequence of numbers starting from the first number in the list through n to the amount in the list counted by my i: 
+for n in range(i + 1, len(nums));
+
+Using the + operation to find what equals
+if nums[n] + nums[i] == target;
+
+Then return [i, n]
+"""
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for n in range(i + 1, len(nums)):
+                if nums[n] + nums[i] == target:
+                    return [i, n]
+        
